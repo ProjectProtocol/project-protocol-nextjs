38b30a32ebe9c33e38e0cd4fabd86d93944a7d98
Tim Sandberg <tasandberg@gmail.com>
2024-06-27 20:00:34 -0700

    Add docs, delete session cookie on signout

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,8 @@
 export const apiURL: string = process.env.API_URL || "";
 
+/**
+ * Api class for making authenticated requests to the API
+ */
 export default class Api {
   private authHeader?: string;
 
