399f75bee0131728ee815b54168992d94f68ffe6
elchao96 <elchao96@gmail.com>
2024-07-13 10:14:09 -0700

    Updating based on Tim's feedback

--- src/app/(main-layout)/account/page.tsx
+++ src/app/(main-layout)/account/page.tsx
@@ -2,9 +2,7 @@ import PageHeader from "@/components/PageHeader";
 import { getTranslations } from "next-intl/server";
 import { getUser, destroySession } from "@/lib/session";
 import { redirect } from "next/navigation";
-import AccountSettingsRow from "./_components/AccountSettingsRow";
-import { Row } from "react-bootstrap";
-import { Button } from "react-bootstrap";
+import AccountSettings from "./_components/AccountSettings";
 
 export default async function Page() {
   const user = await getUser();
@@ -14,25 +12,10 @@ export default async function Page() {
     redirect("/");
   }
 
-  async function handleSignOut() {
-    "use server";
-    return await destroySession();
-  }
-
   return (
     <div>
       <PageHeader title={t("account.title")} showBack />
-      <Row className="gy-4 mt-5">
-        <AccountSettingsRow
-          title={t("shared.email")}
-          detail={user.email}
-          action={
-            <Button variant="outline-dark" size="sm" onClick={handleSignOut}>
-              {t("account.signOut")}
-            </Button>
-          }
-        ></AccountSettingsRow>
-      </Row>
+      <AccountSettings></AccountSettings>
     </div>
   );
 }
