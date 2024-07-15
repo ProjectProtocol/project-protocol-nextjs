9f7c39cac49a8f723e496e54ce1c1d098166391f
elchao96 <elchao96@gmail.com>
2024-07-15 13:03:11 -0700

    Adding Change Password flow to NextJS

--- src/app/(main-layout)/account/_components/AccountSettings.tsx
+++ src/app/(main-layout)/account/_components/AccountSettings.tsx
@@ -5,10 +5,14 @@ import { Button } from "react-bootstrap";
 import { useTranslations } from "next-intl";
 import { destroySession } from "@/lib/session";
 import { useAuth } from "@/components/AuthProvider";
+import { useState } from "react";
+import ChangePasswordModal from "./ChangePasswordModal";
 
 export default function AccountSettings() {
   const { user } = useAuth();
   const t = useTranslations();
+
+  const [showChangePassword, setShowChangePassword] = useState(false);
 
   const signOut = async () => {
     await destroySession();
@@ -17,15 +21,34 @@ export default function AccountSettings() {
   return (
     <Row className="gy-4 mt-5">
       {user && (
-        <AccountSettingsRow
-          title={t("shared.email")}
-          detail={user.email}
-          action={
-            <Button variant="outline-dark" size="sm" onClick={signOut}>
-              {t("account.signOut")}
-            </Button>
-          }
-        />
+        <>
+          <AccountSettingsRow
+            title={t("shared.email")}
+            detail={user.email}
+            action={
+              <Button variant="outline-dark" size="sm" onClick={signOut}>
+                {t("account.signOut")}
+              </Button>
+            }
+          />
+          <AccountSettingsRow
+            title={t("account.changePassword.title")}
+            detail={t("account.changePassword.title")}
+            action={
+              <Button
+                variant="outline-dark"
+                size="sm"
+                onClick={() => setShowChangePassword(true)}
+              >
+                {t("account.changePassword.action")}
+              </Button>
+            }
+          />
+          <ChangePasswordModal
+            show={showChangePassword}
+            onHide={() => setShowChangePassword(false)}
+          />
+        </>
       )}
     </Row>
   );
---
+++ src/app/(main-layout)/account/_components/ChangePasswordModal.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useTranslations } from "next-intl";
+import PopUp, { IPopUp } from "@/components/PopUp";
+import { useFormState } from "react-dom";
+import { changePassword } from "@/lib/actions/account";
+import Input from "@/components/Input";
+import AsyncButton from "@/components/AsyncButton";
+import { getErrorState } from "@/lib/forms";
+import { useEffect } from "react";
+
+interface ICHangePasswordModal {
+  onHide: () => void;
+}
+
+const initialState = {
+  error: undefined,
+  password: "",
+  newPassword: "",
+  newPasswordConfirm: "",
+  errors: {},
+};
+
+export default function ChangePasswordModal({
+  onHide,
+  ...popUpProps
+}: ICHangePasswordModal & IPopUp) {
+  const t = useTranslations();
+  const [state, formAction] = useFormState(changePassword, initialState);
+
+  useEffect(() => {
+    if (!state?.error && !state?.errors) {
+      onHide();
+    }
+  }, [state]);
+
+  return (
+    <PopUp title={t("account.changePassword.title")} {...popUpProps}>
+      {state?.error && (
+        <div className="alert alert-danger" role="alert">
+          {t("account.changePassword.error")}
+        </div>
+      )}
+      <form className="vertical-rhythm" action={formAction}>
+        <p>{t("account.changePassword.intro")}</p>
+        <Input
+          type="password"
+          name="password"
+          label={t("account.changePassword.currentPassword")}
+          placeholder={t("account.changePassword.currentPassword")}
+          {...getErrorState("password", state?.errors)}
+        />
+        <hr />
+        <p>{t("password_reset.modal.intro")}</p>
+        <Input
+          type="password"
+          name="newPassword"
+          label={t("account.changePassword.newPassword")}
+          placeholder={t("account.changePassword.newPassword")}
+          {...getErrorState("newPassword", state?.errors)}
+        />
+        <Input
+          type="password"
+          name="newPasswordConfirm"
+          label={t("account.changePassword.newPasswordConfirm")}
+          placeholder={t("account.changePassword.newPasswordConfirm")}
+          {...getErrorState("newPasswordConfirm", state?.errors)}
+        />
+        <AsyncButton
+          size="lg"
+          className="w-100"
+          variant="primary"
+          type="submit"
+        >
+          {t("account.changePassword.action")}
+        </AsyncButton>
+      </form>
+    </PopUp>
+  );
+}
