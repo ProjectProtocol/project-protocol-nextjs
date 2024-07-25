8f114be68cff2cea8b8c5c79104ab1522830e86f
Tim Sandberg <tasandberg@gmail.com>
2024-07-24 22:36:09 -0700

    Remove old comment

--- src/app/password_resets/[token]/page.tsx
+++ src/app/password_resets/[token]/page.tsx
@@ -1,10 +1,5 @@
 import PasswordResetForm from "./_components/PasswordResetForm";
 
 export default async function Page({ params }: { params: { token: string } }) {
-  // Get token from url param
-  // Pass to form
-  // On success, redirect to login
-  // On failure, show error
-
   return <PasswordResetForm token={params.token} />;
 }
