5fbdfbd96431670a32329f1db7ed05710c2ce4c9
Tim Sandberg <tasandberg@gmail.com>
2024-07-28 11:08:31 -0700

    Tag filters working, useSetSearchParams hook

--- src/app/(main-layout)/resources/page.tsx
+++ src/app/(main-layout)/resources/page.tsx
@@ -4,11 +4,13 @@ import ResourcesList from "./_components/ResourcesList";
 import { Suspense } from "react";
 import ResourceSearchBar from "./_components/ResourceSearchBar";
 import ResourcesLoadingPlaceholder from "./_components/ResourcesLoadingPlaceholder";
+import { ResourceSearchParams } from "@/types/Resource";
+import ResourceFilters from "./_components/ResourceFilters";
 
 export default async function Page({
   searchParams,
 }: {
-  searchParams: { search?: string };
+  searchParams: ResourceSearchParams;
 }) {
   const t = await getTranslations();
 
@@ -16,6 +18,7 @@ export default async function Page({
     <div className="vertical-rhythm">
       <PageHeader title={t("resources.title")} />
       <ResourceSearchBar />
+      <ResourceFilters searchParams={searchParams} />
       <Suspense fallback={<ResourcesLoadingPlaceholder />}>
         <ResourcesList searchText={searchParams.search} />
       </Suspense>
