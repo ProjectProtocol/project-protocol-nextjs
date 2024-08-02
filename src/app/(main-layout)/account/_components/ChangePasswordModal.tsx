ec8686ad57433bd6f3904561f5c4f4aea83dce3a
elchao96 <elchao96@gmail.com>
2024-08-02 12:18:51 -0400

    Moving all instances of useFormState to useForm and client-side zod

--- src/app/(main-layout)/account/_components/ChangePasswordModal.tsx
+++ src/app/(main-layout)/account/_components/ChangePasswordModal.tsx
@@ -2,25 +2,22 @@
 
 import { useTranslations } from "next-intl";
 import PopUp, { IPopUp } from "@/components/PopUp";
-import { useFormState } from "react-dom";
-import { changePassword } from "@/lib/actions/account";
+import {
+  changePassword,
+  IChangePasswordFormState,
+} from "@/lib/actions/account";
 import Input from "@/components/Input";
 import AsyncButton from "@/components/AsyncButton";
-import { getErrorState } from "@/lib/forms";
-import { useEffect } from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
+import { useCallback } from "react";
 
 interface IChangePasswordModal {
   onHide: () => void;
   closeButton?: boolean;
 }
-
-const initialState = {
-  error: undefined,
-  password: "",
-  newPassword: "",
-  newPasswordConfirm: "",
-  errors: {},
-};
 
 export default function ChangePasswordModal({
   onHide,
@@ -28,13 +25,47 @@ export default function ChangePasswordModal({
   ...popUpProps
 }: IChangePasswordModal & IPopUp) {
   const t = useTranslations();
-  const [state, formAction] = useFormState(changePassword, initialState);
+  const schema = z
+    .object({
+      password: z.string(),
+      newPassword: z.string().min(8, t("account.changePassword.detail")),
+      newPasswordConfirm: z.string().min(8, t("account.changePassword.detail")),
+    })
+    .refine((data) => data.newPassword === data.newPasswordConfirm, {
+      message: t("password_reset.newPasswordConfirmMismatch"),
+      path: ["newPasswordConfirm"],
+    });
+  const { register, handleSubmit, getFieldState, formState } =
+    useForm<IChangePasswordFormState>({
+      mode: "onBlur",
+      defaultValues: {
+        password: "",
+        newPassword: "",
+        newPasswordConfirm: "",
+      },
+      resolver: zodResolver(schema),
+    });
 
-  useEffect(() => {
-    if (!state?.error && !state?.errors) {
+  async function onSubmit(data: IChangePasswordFormState) {
+    const { error } = await changePassword(data);
+    if (error) {
+      toast.error(t(error));
+    } else {
+      toast.success(t("account.changePassword.success.heading"));
       onHide();
     }
-  }, [state]);
+  }
+
+  const validationProps = useCallback(
+    (fieldName: keyof IChangePasswordFormState) => {
+      const { invalid, error } = getFieldState(fieldName, formState);
+      return {
+        isInvalid: invalid,
+        error: error?.message,
+      };
+    },
+    [getFieldState, formState]
+  );
 
   return (
     <PopUp
@@ -43,41 +74,38 @@ export default function ChangePasswordModal({
       closeButton
       onHide={onHide}
     >
-      {state?.error && (
-        <div className="alert alert-danger" role="alert">
-          {t("account.changePassword.error")}
-        </div>
-      )}
-      <form className="vertical-rhythm" action={formAction}>
+      <form className="vertical-rhythm" onSubmit={handleSubmit(onSubmit)}>
         <p>{t("account.changePassword.intro")}</p>
         <Input
           type="password"
-          name="password"
           label={t("account.changePassword.currentPassword")}
           placeholder={t("account.changePassword.currentPassword")}
-          {...getErrorState("password", state?.errors)}
+          {...validationProps("password")}
+          {...register("password")}
         />
         <hr />
         <p>{t("password_reset.modal.intro")}</p>
         <Input
           type="password"
-          name="newPassword"
           label={t("account.changePassword.newPassword")}
           placeholder={t("account.changePassword.newPassword")}
-          {...getErrorState("newPassword", state?.errors)}
+          {...validationProps("newPassword")}
+          {...register("newPassword")}
         />
         <Input
           type="password"
-          name="newPasswordConfirm"
           label={t("account.changePassword.newPasswordConfirm")}
           placeholder={t("account.changePassword.newPasswordConfirm")}
-          {...getErrorState("newPasswordConfirm", state?.errors)}
+          {...validationProps("newPasswordConfirm")}
+          {...register("newPasswordConfirm")}
         />
         <AsyncButton
           size="lg"
           className="w-100"
           variant="primary"
           type="submit"
+          disabled={!formState.isValid}
+          loading={formState.isSubmitting}
         >
           {t("account.changePassword.action")}
         </AsyncButton>
--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -2,43 +2,64 @@
 
 import Input from "../Input";
 import { useTranslations } from "next-intl";
-import { useFormState } from "react-dom";
 import AsyncButton from "../AsyncButton";
-import { login } from "@/lib/actions/auth";
-
-type ErrorState = {
-  error?: string;
-  isInvalid: boolean;
-};
-function getErrorState(fieldName: string, errors: any): ErrorState {
-  return {
-    error: errors?.[fieldName],
-    isInvalid: !!errors?.[fieldName],
-  };
-}
-
-const initialState = {
-  error: undefined,
-  errors: {},
-};
+import { login, ILoginFormState } from "@/lib/actions/auth";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback } from "react";
 
 export default function LoginForm({ callbackURL }: { callbackURL?: string }) {
   const t = useTranslations();
-  const [state, formAction] = useFormState(login, initialState);
+  const schema = z.object({
+    email: z
+      .string()
+      .min(1, t("login.emailRequired"))
+      .email(t("login.emailMessage")),
+    password: z
+      .string()
+      .min(1, t("login.passwordRequired"))
+      .min(8, t("shared.passwordLengthError")),
+  });
+  const { register, handleSubmit, getFieldState, formState } =
+    useForm<ILoginFormState>({
+      mode: "onBlur",
+      defaultValues: {
+        loginEmail: "",
+        password: "",
+        callbackURL,
+      },
+      resolver: zodResolver(schema),
+    });
+
+  async function onSubmit(data: ILoginFormState) {
+    await login(data);
+  }
+
+  const validationProps = useCallback(
+    (fieldName: keyof ILoginFormState) => {
+      const { invalid, error } = getFieldState(fieldName, formState);
+      return {
+        isInvalid: invalid,
+        error: error?.message,
+      };
+    },
+    [getFieldState, formState]
+  );
 
   return (
     <div className="d-block p-4">
       <div className="text-center mb-3">{t("login.loginTitleHelper")}</div>
-      <form className="vertical-rhythm" action={formAction}>
+      <form className="vertical-rhythm" onSubmit={handleSubmit(onSubmit)}>
         <input type="hidden" name="callbackURL" value={callbackURL} />
         <Input
           size="lg"
           controlId={`login-email`}
           label={t("login.email")}
           type="email"
-          name="email"
           placeholder={t("login.emailPlaceholder")}
-          {...getErrorState("email", state?.errors)}
+          {...register("loginEmail")}
+          {...validationProps("loginEmail")}
         />
         <Input
           size="lg"
@@ -46,8 +67,8 @@ export default function LoginForm({ callbackURL }: { callbackURL?: string }) {
           label={t("login.password")}
           className="mb-2"
           type="password"
-          name="password"
-          {...getErrorState("password", state?.errors)}
+          {...register("password")}
+          {...validationProps("password")}
         />
         <div>
           <a className="link" href="/login/forgot-password">
@@ -59,6 +80,8 @@ export default function LoginForm({ callbackURL }: { callbackURL?: string }) {
           className="w-100"
           variant="primary"
           type="submit"
+          disabled={!formState.isValid}
+          loading={formState.isSubmitting}
         >
           {t("login.login")}
         </AsyncButton>
