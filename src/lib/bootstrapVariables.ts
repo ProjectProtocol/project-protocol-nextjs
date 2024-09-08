c20f50c86648ca39f4f1fa29bc3ffcd4a5c3d94b
Tim Sandberg <tasandberg@gmail.com>
2024-09-08 10:14:23 -0700

    Remove broken sass variable export module

--- src/lib/bootstrapVariables.ts
+++ src/lib/bootstrapVariables.ts
@@ -1,5 +1,3 @@
-import rawBootstrapVariables from "../styles/bootstrap-theme/export.module.scss";
-
 // Dasherized theme color names as they appear in the sass stylesheets
 export const themeColors = [
   "primary",
@@ -24,17 +22,3 @@ export const themeColors = [
 ] as const;
 
 export type ThemeColor = (typeof themeColors)[number];
-type BootstrapVariables = Record<ThemeColor, string>;
-
-const bootstrapVariables: BootstrapVariables =
-  rawBootstrapVariables as BootstrapVariables;
-
-export const ratingColors = [
-  bootstrapVariables.rating1,
-  bootstrapVariables.rating2,
-  bootstrapVariables.rating3,
-  bootstrapVariables.rating4,
-  bootstrapVariables.rating5,
-];
-
-export default bootstrapVariables;
