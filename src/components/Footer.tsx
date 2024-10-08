c939b5768d8fed5c6a4214a341594460fa467195
elchao96 <elchao96@gmail.com>
2024-10-07 20:26:11 -0700

    Various footer and UI fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,6 +11,10 @@ export default async function Footer() {
       label: t("whatIsProjectProtocol"),
     },
     {
+      url: urlPrefix("how-it-works"),
+      label: t("howItWorks"),
+    },
+    {
       url: urlPrefix("the-team"),
       label: t("theTeam"),
     },
@@ -23,6 +27,11 @@ export default async function Footer() {
       label: t("termsOfService"),
     },
     { url: urlPrefix("contact-us"), label: t("contact") },
+    {
+      url: `https://youngwomenfree.app.neoncrm.com/forms/project-protocol`,
+      label: t("donate"),
+      target: "_blank",
+    },
   ];
 
   return (
@@ -30,11 +39,12 @@ export default async function Footer() {
       className="bg-dark text-center mt-auto py-md-4 pt-4"
       style={{ paddingBottom: "100px" }}
     >
-      {links.map(({ label, url }) => (
+      {links.map(({ label, url, target }) => (
         <Link
           key={`footer-link-${label}`}
           className="mx-3 my-3 d-block d-md-inline text-center link-white link-underline-opacity-0"
           href={url}
+          target={target || "_self"}
         >
           {label}
         </Link>
