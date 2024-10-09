756ee0d11702db304ab69f0c047dfcdb1c4ba7ae
Tim Sandberg <tasandberg@gmail.com>
2024-10-08 23:40:01 -0700

    Mobile landing page styles. TODO: sticky dark header.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ import landingImage from "@/../public/images/landing-image.jpeg";
 import VideoComponent from "./_components/VideoComponent";
 import { CldImage } from "next-cloudinary";
 import LandingHeroImage from "./_components/LandingHeroImage";
+import LandingPageHeader from "./_components/LandingPageHeader";
 
 const MAX_WIDTH = 720;
 
@@ -27,7 +28,7 @@ function LandingPageSegment({
   classes?: string;
   children: React.ReactNode;
 }) {
-  return <section className={"py-5 " + classes}>{children}</section>;
+  return <section className={"py-3 " + classes}>{children}</section>;
 }
 
 export default async function Page() {
@@ -39,67 +40,42 @@ export default async function Page() {
       <div className={`position-relative w-100 ${styles.topImageContainer}`}>
         <LandingHeroImage />
         <div className={styles.imageOverlay} />
-
-        <Navbar>
-          <Container fluid>
-            <NavbarBrand
-              as={Link}
-              href="/"
-              className="d-flex flex-row align-items-center"
-            >
-              <div className="d-md-none">
-                <Image
-                  priority
-                  src="/images/icon.svg"
-                  width="20"
-                  height="20"
-                  className="me-2"
-                  alt={"Project Protocol logo"}
-                />
-              </div>
-              <div className="d-none d-md-block me-2">
-                <Image
-                  priority
-                  unoptimized
-                  src="/images/icon.svg"
-                  width="0"
-                  height="0"
-                  style={{ width: "25px", height: "auto" }}
-                  alt={"Project Protocol logo"}
-                />
-              </div>
-              <span
-                className={
-                  "w-100 d-md-inline fw-medium pe-auto text-white " +
-                  styles.navbarTitle
-                }
-                style={{ letterSpacing: -0.5 }}
-              >
-                Project Protocol
-              </span>
-            </NavbarBrand>
-            <LocaleSwitcher locale={locale} dark />
-          </Container>
-        </Navbar>
+        <LandingPageHeader locale={locale} />
+        <div
+          className="position-absolute text-white w-100 d-md-none"
+          style={{ bottom: 0 }}
+        >
+          <div className="d-flex flex-column justy-content-center align-items-center p-3">
+            <h1>{t("home.welcomeTitle")}</h1>
+            <p>
+              {t("home.welcomeMessage")}.{" "}
+              <Link href="/content/en-US/about" className="link-white">
+                {t("shared.learnMore")}
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="content-page">
-        <LandingPageSegment classes="text-center px-4 px-md-0">
+      <div className="content-page mt-0">
+        {/* Desktop Explore Segment */}
+        <LandingPageSegment classes="text-center px-4 px-md-0 d-none d-md-block">
           <div className="m-auto" style={{ maxWidth: 720 }}>
             <h1>{t("home.welcomeTitle")}</h1>
             <p>
               {t("home.welcomeMessage")}.{" "}
               <Link href="/content/en-US/about">{t("shared.learnMore")}</Link>
             </p>
-            <div className="d-none d-md-block">
+            <div>
               <IconLinks locale={locale} classes="py-3 " iconHeight={75} />
             </div>
           </div>
         </LandingPageSegment>
-        <LandingPageSegment classes="bg-white text-center px-4 d-md-none">
-          <h2>{t("home.explore")}</h2>
-          <IconLinks locale={locale} classes="py-3" />
+        {/* Mobile explore segment */}
+        <LandingPageSegment classes="text-center px-4 d-md-none mt-0">
+          <h2 className="mb-3">{t("home.explore")}</h2>
+          <IconLinks locale={locale} />
         </LandingPageSegment>
-        <LandingPageSegment classes="text-center px-3 bg-md-white">
+        <LandingPageSegment classes="text-center px-3 bg-white">
           <h2>{t("home.learnHowToRate")}</h2>
           <div className="h-100 w-100">
             <VideoComponent />
