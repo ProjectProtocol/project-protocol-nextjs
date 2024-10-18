7cca52640c19a86fef1e7b45dc79f970ff82518b
Tim Sandberg <tasandberg@gmail.com>
2024-10-17 18:49:31 -0700

    Merge component changes from main

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,25 +1,23 @@
+"use client";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavItem from "react-bootstrap/NavItem";
 import Image from "next/image";
-import { getTranslations } from "next-intl/server";
-import MenuLink from "./MenuLink";
-import User from "@/types/User";
-import { Link } from "@/i18n/routing";
+import Link from "next/link";
 import LocaleSwitcher from "../LocaleSwitcher";
+import MenuLink from "./MenuLink";
+import { useLocale, useTranslations } from "next-intl";
+import { useAuth } from "../AuthProvider";
 
 export const MENU_MAX_WIDTH = 1048;
 
-export default async function Menu({
-  locale,
-  user,
-}: {
-  locale: string;
-  user?: User;
-}) {
-  const t = await getTranslations();
+export default function Menu() {
+  const { user } = useAuth();
+  const tHome = useTranslations("home");
+  const tNavigation = useTranslations("navigation");
+  const locale = useLocale();
 
   return (
     <Navbar className="bg-black flex-column py-0 overflow-hidden" sticky="top">
@@ -37,35 +35,33 @@ export default async function Menu({
           style={{ maxWidth: MENU_MAX_WIDTH }}
           className="py-2 py-md-3 d-flex justify-content-between align-items-center"
         >
-          <NavbarBrand>
-            <Link href="/" className="text-decoration-none">
-              <div
-                className="d-flex flex-row justify-content-center align-items-center"
-                role="button"
-              >
-                <Image
-                  priority
-                  unoptimized
-                  src={"/images/icon.svg"}
-                  width="0"
-                  height="0"
-                  className="me-2"
-                  style={{ width: 28, height: 28 }}
-                  alt={"Project Protocol logo"}
-                />
-                <span className="fs-2 w-100 d-md-inline fw-semibold pe-auto text-white text-md-black">
-                  Project Protocol
-                </span>
-              </div>
-            </Link>
+          <NavbarBrand as={Link} href="/">
+            <div
+              className="d-flex flex-row justify-content-center align-items-center"
+              role="button"
+            >
+              <Image
+                priority
+                unoptimized
+                src={"/images/icon.svg"}
+                width="0"
+                height="0"
+                className="me-2"
+                style={{ width: 28, height: 28 }}
+                alt={"Project Protocol logo"}
+              />
+              <span className="fs-2 w-100 d-md-inline fw-semibold pe-auto text-white text-md-black">
+                Project Protocol
+              </span>
+            </div>
           </NavbarBrand>
           <Nav className="fs-4 d-none d-md-flex align-items-center gap-2 fw-semibold">
-            <MenuLink className="m-0" href="/" exact>
-              {t("home.title")}
+            <MenuLink as={Link} className="m-0" href="/" exact>
+              {tHome("title")}
             </MenuLink>
-            <MenuLink href="/rate-my-po">{t("navigation.rateMyPo")}</MenuLink>
+            <MenuLink href="/rate-my-po">{tNavigation("rateMyPo")}</MenuLink>
             <MenuLink className="m-0" href="/resources">
-              {t("navigation.resources")}
+              {tNavigation("resources")}
             </MenuLink>
             {user ? (
               <MenuLink className="m-0" href="/account" title="Account">
@@ -84,7 +80,7 @@ export default async function Menu({
                   href="/auth/signup"
                   scroll={false}
                 >
-                  {t("navigation.signUp")}
+                  {tNavigation("signUp")}
                 </Link>
               </NavItem>
             )}
