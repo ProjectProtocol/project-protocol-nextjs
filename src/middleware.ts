4bcf02d9c03441935424aa8874e7f678ed3d4156
Tim Sandberg <tasandberg@gmail.com>
2024-10-18 08:51:28 -0700

    Add back auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,30 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { getUser } from "./lib/session";
 
+const AUTH_REDIRECTS = ["/auth/login", "/auth/signup", "/auth/forgot-password"];
+
+const AUTH_REQUIRED = ["/account", "/rate-my-po/new"];
 const intlMiddleware = createMiddleware(routing, { alternateLinks: false });
-
 export async function middleware(request: NextRequest) {
+  if (AUTH_REQUIRED.includes(request.nextUrl.pathname)) {
+    const user = await getUser();
+    if (!user) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  if (AUTH_REDIRECTS.includes(request.nextUrl.pathname)) {
+    const user = await getUser();
+    if (user) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
   return intlMiddleware(request);
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|images|.*\\..*).*)"],
+  matcher: "/((?!api|_next/static|favicon.ico|images).*)",
 };
