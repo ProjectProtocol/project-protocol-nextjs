28da11e2bc1e7a8f7752354f3877f98f8b7372f3
Tim Sandberg <tasandberg@gmail.com>
2024-11-03 10:40:45 -0800

    Add icons to middleware ignore list

--- src/middleware.ts
+++ src/middleware.ts
@@ -38,5 +38,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher:
-    "/((?!api|_next/static|favicon.ico|images|monitoring|sitemap|opengraph-image|manifest).*)",
+    "/((?!api|_next/static|favicon.ico|images|monitoring|sitemap|opengraph-image|manifest|icon).*)",
 };
