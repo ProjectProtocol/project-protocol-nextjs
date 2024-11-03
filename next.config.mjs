f9e4c091b57e901854244c8ec46a4ff3bfdde4d7
Tim Sandberg <tasandberg@gmail.com>
2024-11-03 10:30:01 -0800

    Fix manifest redirect (ignore in middleware)

--- next.config.mjs
+++ next.config.mjs
@@ -62,11 +62,6 @@ export default withSentryConfig(nextConfig, {
     enabled: true,
   },
 
-  // Disable sourcemap upload outside of production
-  sourcemaps: {
-    disable: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT !== "production",
-  },
-
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
